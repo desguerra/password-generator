646b84bb5a3b20e3198808f2d565e553cbee64a7
desguerra <ddesguerra@ucdavis.edu>
2022-07-20 13:56:59 -0700

    set up prompts for password criteria

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,40 @@
 // Assignment code here
 
+// generate password function
+function generatePassword() {
+    // make new variable for generated password
+    var newPassword = "testing - hello, World!";
+
+    // if length of pw is NOT between 8 and 128 char, try again (loop back)
+    var charLimit = window.prompt("How many characters would you like your password to contain?");
+    charLimit = parseInt(charLimit);
+    if (charLimit < 8 || charLimit > 128) {
+        window.alert("Sorry! Password must be between 8 - 128 characters. Please enter a valid password length.")
+        generatePassword();
+    }
+    // else, continue
+
+    // if lowercase
+    var isLower = window.confirm("Click OK to confirm including lowercase characters.");
+
+    // if uppercase
+    var isUpper = window.confirm("Click OK to confirm including uppercase characters.");
+
+    // if numeric
+    var isNumeric = window.confirm("Click OK to confirm including numeric characters.");
+
+    // if special characters
+    var isSpecialChar = window.confirm("Click OK to confirm including special characters.");
+
+    // if NONE OF THE ABOVE, then try again (loop back)
+    if (!isLower && !isUpper && !isNumeric && !isSpecialChar) {
+        window.alert("Must select at least once character type. Please try again.");
+        generatePassword();
+    }
+
+    // finally, return a generated password that matches selected criteria
+    return newPassword;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,7 +46,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
