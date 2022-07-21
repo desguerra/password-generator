e6cb53782199150b37472b9788a8871ac42c9daf
desguerra <ddesguerra@ucdavis.edu>
2022-07-20 17:51:48 -0700

    fix invalid input bug

--- assets/script.js
+++ assets/script.js
@@ -34,58 +34,57 @@ function generatePassword() {
     // make new variable for generated password
     var newPassword = "";
 
-    debugger;
-
     // if length of pw is NOT between 8 and 128 char, try again (loop back)
     var charLimit = window.prompt("How many characters would you like your password to contain?");
     charLimit = parseInt(charLimit);
-    if (charLimit < 8 || charLimit > 128) {
+
+    if (charLimit > 7 || charLimit < 129) {
+        // is lowercase?
+        var isLower = window.confirm("Click OK if you would like to include lowercase characters.");
+
+        // is uppercase?
+        var isUpper = window.confirm("Click OK if you would like to include uppercase characters.");
+
+        // is numeric?
+        var isNumeric = window.confirm("Click OK if you would like to include numeric characters.");
+
+        // is special characters?
+        var isSpecialChar = window.confirm("Click OK if you would like to include special characters.");
+
+        // if NONE OF THE ABOVE, then try again (loop back)
+        if (!isLower && !isUpper && !isNumeric && !isSpecialChar) {
+            window.alert("Must select at least once character type. Please try again.");
+            return newPassword;
+        }
+        else { // else, return a generated password that matches selected criteria
+            
+            for (var i = 0; i < charLimit; i++) {
+
+                // randomize the characters that go into the new pw, given the selected criteria
+                var random = Math.floor(Math.random() * 4 + 1);
+                if (random === 1 && isLower) {
+                    newPassword += getLower();
+                }
+                else if (random === 2 && isUpper) {
+                    newPassword += getUpper();
+                }
+                else if (random === 3 && isNumeric) {
+                    newPassword += getNumeric();
+                }
+                else if (random === 4 && isSpecialChar) {
+                    newPassword += getSpecialChar();
+                }
+                else {
+                    // if none of the above, then loop again
+                    charLimit += 1;
+                }
+            };
+
+            return newPassword;
+        }
+    }
+    else { // else, continue
         window.alert("Sorry! Password must be between 8 - 128 characters. Please enter a valid password length.")
-        return newPassword;
-    }
-    // else, continue
-
-    // is lowercase?
-    var isLower = window.confirm("Click OK if you would like to include lowercase characters.");
-
-    // is uppercase?
-    var isUpper = window.confirm("Click OK if you would like to include uppercase characters.");
-
-    // is numeric?
-    var isNumeric = window.confirm("Click OK if you would like to include numeric characters.");
-
-    // is special characters?
-    var isSpecialChar = window.confirm("Click OK if you would like to include special characters.");
-
-    // if NONE OF THE ABOVE, then try again (loop back)
-    if (!isLower && !isUpper && !isNumeric && !isSpecialChar) {
-        window.alert("Must select at least once character type. Please try again.");
-        return newPassword;
-    }
-    else { // else, return a generated password that matches selected criteria
-        
-        for (var i = 0; i < charLimit; i++) {
-
-            // randomize the characters that go into the new pw, given the selected criteria
-            var random = Math.floor(Math.random() * 4 + 1);
-            if (random === 1 && isLower) {
-                newPassword += getLower();
-            }
-            else if (random === 2 && isUpper) {
-                newPassword += getUpper();
-            }
-            else if (random === 3 && isNumeric) {
-                newPassword += getNumeric();
-            }
-            else if (random === 4 && isSpecialChar) {
-                newPassword += getSpecialChar();
-            }
-            else {
-                // if none of the above, then loop again
-                charLimit += 1;
-            }
-        };
-
         return newPassword;
     }
 };
